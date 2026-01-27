aeebaa28514b72038c5cf9a53577680f521154f9
medsrk <rcpkarrinton@hotmail.co.uk>
2026-01-27 14:01:30 +0000

    Adds content type header for video endpoint
    Ensures that the video endpoint returns JSON data by explicitly setting the "Content-Type" header to "application/json".

--- internal/handlers/video.go
+++ internal/handlers/video.go
@@ -54,6 +54,7 @@ func (h *VideoHandler) GetVideo(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(video)
 }
 
